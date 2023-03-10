50fb760257c6a38c234838db53381938f15e89b6
Omar Parra <omarfdez92@gmail.com>
2023-03-09 22:01:16 -0600

    Already addd moreless the setup for the navigate part, currently working on see how is that in navigation I dont see the layouts to add them. Also see if we have the correct build configuration and will continue with this part

--- TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.triviaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.triviaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
